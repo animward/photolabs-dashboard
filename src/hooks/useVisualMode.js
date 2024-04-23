44f46d53718863d59fcf357c80ff61790e2cca1c
animward <animward@gmail.com>
2024-04-23 16:46:33 -0700

    add transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,7 +4,11 @@ import React, { useState } from 'react';
 const useVisualMode = (initialMode) => {
   const [mode, setMode] = useState(initialMode);
 
-  return { mode };
+  const transition = (newMode) => {
+    setMode(newMode);
+  };
+
+  return { mode, transition };
 };
 
 export default useVisualMode;
